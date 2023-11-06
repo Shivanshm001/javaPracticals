d07d3f3ac5f66cf4cbaeb38ff387f861cdceaf72
Shivansh mishra <Shivanshmishra.chd@gmail.com>
2023-11-06 18:36:41 +0530

    Completed 32

--- src/32.java
+++ src/32.java
@@ -1,7 +1,39 @@
 //String class
 
 class Main{
+    private static void print(Object msg){
+        System.out.println(msg);
+    }
     public static void main(String[] args) {
-        String text = "";
+        String text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+        String firstName = "Shivansh";
+        String lastName = "Mishra";
+
+
+        print("To get the length of the string : ");
+        print(text.length());
+        print("To get a character at specific index : ");
+        print(text.charAt(4));
+        print("To concatinate two strings : ");
+        print(firstName.concat(lastName));
+        print("Get a substring of a string : ");
+        print(firstName.substring(4));
+        print(firstName.substring(4,8));
+        print("Get index of a character 'i' :");
+        print(firstName.indexOf("i"));
+        print("Check if a string contains a specific sequence : ");
+        print(text.contains("dummy"));
+        print(text.contains("shivansh"));
+        print("Compare two strings : ");
+        print(firstName.equals(lastName));
+        print("Covert to uppercase : ");
+        print(firstName.toUpperCase());
+        print("Covert to lowercase : ");
+        print(firstName.toLowerCase());
+        print("Convert a string into an array of type sting : ");
+        print("First name : "+ firstName);
+
+        String[] strArray = firstName.split("");
+        for(String c : strArray) print(c);
     }
 }
